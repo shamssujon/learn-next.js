e95b5aae5dc1c2280a06885dc1bf426da0f7d6c7
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-01-13 07:21:21 +0600

    Next Link

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,13 @@
+import Link from "next/link";
 
 const blog = () => {
     return (
         <div>
             <h1>This is blog page</h1>
+            <nav>
+                <Link href="/blog/articles/article-1">Article 1</Link>
+                <Link href="/blog/articles/article-2">Article 2</Link>
+            </nav>
         </div>
     );
 };
