0f80d171e2a3d97cd1ccf7198d266c51cb6ae177
Shamsul Islam Sujon <sujon2762@gmail.com>
2023-01-13 07:26:05 +0600

    Next Head - Dynamic page title

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,16 @@
+import Head from "next/head";
 
 const about = () => {
-    return (
-        <div>
-            <h1>This is about page</h1>
-        </div>
-    );
+	return (
+		<>
+			<Head>
+				<title>About page</title>
+			</Head>
+			<div>
+				<h1>This is about page</h1>
+			</div>
+		</>
+	);
 };
 
 export default about;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 const blog = () => {
     return (
         <div>
+            <Head>
+                <title>Blog Page</title>
+            </Head>
             <h1>This is blog page</h1>
             <nav>
                 <Link href="/blog/articles/article-1">Article 1</Link>
